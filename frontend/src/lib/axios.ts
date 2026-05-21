4fce389ae3fbd83b78d7bbdb3cb218c10a73215e
Wahaj Ul Haq <ulhaqwahaj@gmail.com>
2026-05-21 07:24:21 +0500

    s

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosError, type AxiosInstance } from "axios";
 
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000";
+  // process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000";
+  process.env.NEXT_PUBLIC_API_URL ?? "https://backend.chainflow.codexflow.co";
 
 export const api: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
